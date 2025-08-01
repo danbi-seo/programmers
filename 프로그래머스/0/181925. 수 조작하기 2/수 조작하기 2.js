dfc3cc2f897973b3117fd894adb8083ac0a301e3
Remi <sdb0504@naver.com>
2025-08-01 23:57:24 +0900

    Update 수 조작하기 2.js

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -2,21 +2,20 @@ function solution(numLog) {
   var answer = "";
 
   for (let i = 1; i < numLog.length; i++) {
-    // numLogp -> numLog로 수정
     const dan = numLog[i] - numLog[i - 1];
 
     switch (dan) {
       case 1:
-        answer += "w"; // anwer -> answer로 수정
+        answer += "w"; 
         break;
       case -1:
-        answer += "s"; // anwer -> answer로 수정
+        answer += "s";
         break;
       case 10:
-        answer += "d"; // anwer -> answer로 수정
+        answer += "d";
         break;
       case -10:
-        answer += "a"; // anwer -> answer로 수정
+        answer += "a";
         break;
     }
   }
